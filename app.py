324d4498e1cdb9f96013db7f3ebfda751258ba6b
Nguyen Phuc <nguyentruongphuc@gmail.com>
2025-06-25 13:24:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def add_text(state, message, system_prompt, request: gr.Request):
         disable_btn,
     ) * 5
 
-model_name = "5CD-AI/Vintern-3B-beta"
+model_name = "5CD-AI/Vintern-3B-R-beta"
 model = AutoModel.from_pretrained(
     model_name,
     torch_dtype=torch.bfloat16,
